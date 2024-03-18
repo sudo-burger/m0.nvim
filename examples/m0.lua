83cfc3b1b24399fa788f57bdb6dab601fc8cad86
yk <federico@pietrolucci.net>
2024-03-18 21:43:19 +0100

    Example configuration: extended and cleaned up.

--- examples/m0.lua
+++ examples/m0.lua
@@ -1,17 +1,33 @@
 return {
 	"sudo-burger/m0.nvim",
-	cmd = { "Mchat" },
+	cmd = { "M0chat" },
+	init = function()
+		vim.keymap.set("n", "<leader>x", require("m0").M0chat, { desc = "m0.nvim" })
+	end,
 	config = function()
-		vim.keymap.set("n", "<C-d>", require("m0").Mchat, { desc = "m0.nvim" })
-		-- require("m").setup({ default_backend = "openai-0" })
-		-- default_backend = require('m').make_openai {
-		--   url = 'https://api.openai.com/v1/chat/completions',
-		--   api_key = require('m').get_api_key 'api.openai.com/key-0',
-		--   model = 'gpt-3.5-turbo',
-		--   max_tokens = 100,
-		--   temperature = 0.7,
-		--   prompt = 'You are literally The Hulk.',
-		-- },
-		-- })
+		require("m0").setup({
+			backends = {
+				["Charles Bukowski"] = {
+					type = "openai",
+					url = "https://api.openai.com/v1/chat/completions",
+					api_key = require("m0").get_api_key("api.openai.com/key-0"),
+					model = "gpt-3.5-turbo",
+					max_tokens = 100,
+					temperature = 0.7,
+					prompt = "You are now Charles Bukowski.",
+				},
+				["Marilyn Monroe"] = {
+					type = "anthropic",
+					url = "https://api.anthropic.com/v1/messages",
+					api_key = require("m0").get_api_key("api.anthropic.com/key-0"),
+					anthropic_version = "2023-06-01",
+					model = "claude-3-haiku-20240307",
+					max_tokens = 100,
+					temperature = 0.7,
+					prompt = "Assume the role or Marilyn Monroe.",
+				},
+			},
+			default_backend = "Charles Bukowski",
+		})
 	end,
 }

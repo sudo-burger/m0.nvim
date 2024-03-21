61168cf61cee622c2bc6ffe1824c2d958d74277d
yk <federico@pietrolucci.net>
2024-03-21 21:50:04 +0100

    Examples: Mistral config.

--- examples/m0.lua
+++ examples/m0.lua
@@ -9,19 +9,21 @@ return {
 			backends = {
 				["openai:gpt-3.5-turbo"] = {
 					type = "openai",
-					url = "https://api.openai.com/v1/chat/completions",
 					api_key = require("m0").get_api_key("api.openai.com/key-0"),
 					model = "gpt-3.5-turbo",
-					max_tokens = 100,
-					temperature = 0.7,
 				},
 				["anthropic:claude-3-haiku"] = {
 					type = "anthropic",
-					url = "https://api.anthropic.com/v1/messages",
 					api_key = require("m0").get_api_key("api.anthropic.com/key-0"),
 					anthropic_version = "2023-06-01",
 					model = "claude-3-haiku-20240307",
-					max_tokens = 100,
+				},
+				["mistral:mistral-large-latest"] = {
+					type = "openai",
+					url = "https://api.mistral.ai/v1/chat/completions",
+					api_key = require("m0").get_api_key("api.mistral.ai/key-0"),
+					model = "mistral-large-latest",
+					max_tokens = 1024,
 					temperature = 0.7,
 				},
 			},

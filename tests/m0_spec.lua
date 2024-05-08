16d3a14e12c6da2bf80f164e23c67b46c276e56e
yk <federico@pietrolucci.net>
2024-05-08 22:54:45 +0200

    Test: added a bunch of tests.

--- tests/m0_spec.lua
+++ tests/m0_spec.lua
@@ -1,4 +1,4 @@
----@diagnostic disable: undefined-global
+---@diagnostic disable: undefined-global, undefined-field
 
 Opts = {
   backends = {
@@ -41,6 +41,12 @@ describe('m0', function()
 end)
 
 describe('m0', function()
+  it('can debug', function()
+    M0.debug()
+  end)
+end)
+
+describe('m0', function()
   it('has an initial prompt', function()
     assert(M0.get_current_prompt() == Opts.prompts[Opts.default_prompt_name])
   end)
@@ -71,21 +77,50 @@ end)
 describe('m0', function()
   it('can change the backend', function()
     local new_backend_name = 'mistral:mistral-large-latest'
+    local ibackend_opts = vim.inspect(Opts.backends[Opts.default_backend_name])
     local ebackend_opts = vim.inspect(Opts.backends[new_backend_name])
-    M0.M0backend(new_backend_name)
-    local abackend_opts = vim.inspect(M0.get_current_backend_opts() or {})
-    assert(
-      (ebackend_opts ~= vim.inspect {}) and (ebackend_opts == abackend_opts),
-      'Expected: '
-        .. ebackend_opts
-        .. ' where this is not {}. Actual: '
-        .. abackend_opts
-    )
+    describe(': the initial backend', function()
+      it('is not empty', function()
+        assert(ibackend_opts ~= vim.inspect {})
+      end)
+    end)
+    describe(': the expected backend', function()
+      it('is not empty', function()
+        assert(ebackend_opts ~= vim.inspect {})
+      end)
+    end)
+    describe(': the initial and expected backends', function()
+      it('are not equal', function()
+        assert(
+          ibackend_opts ~= ebackend_opts,
+          'initial backend is the same as the test backend (should not happen).'
+        )
+      end)
+    end)
+    describe(': the expected and actual backends', function()
+      it('are equal', function()
+        M0.M0backend(new_backend_name)
+        local abackend_opts = vim.inspect(M0.get_current_backend_opts() or {})
+        assert(
+          ebackend_opts == abackend_opts,
+          'Expected: ' .. ebackend_opts .. ' Actual: ' .. abackend_opts
+        )
+      end)
+    end)
+    describe(': restoring the default backend', function()
+      it('can restore', function() end)
+      M0.M0backend(Opts.default_backend_name)
+      local abackend_opts = vim.inspect(M0.get_current_backend_opts() or {})
+      assert(
+        ibackend_opts == abackend_opts,
+        'Expected: ' .. ibackend_opts .. ' Actual: ' .. abackend_opts
+      )
+    end)
   end)
 end)
 
 describe('m0', function()
-  it('can debug', function()
-    M0.debug()
+  it('can chat', function()
+    M0.M0chat()
   end)
 end)

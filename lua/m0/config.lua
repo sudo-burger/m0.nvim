9f14a1859ac2b2e945f2707c8536562e2097433b
yk <federico@pietrolucci.net>
2024-06-22 15:02:25 +0200

    Fix: updated Anthropic models list.

--- lua/m0/config.lua
+++ lua/m0/config.lua
@@ -35,8 +35,7 @@ M = {
         url = 'https://api.anthropic.com/v1/messages',
         models = {
           'claude-3-haiku-20240307',
-          'claude-3-haiku-20240307',
-          'claude-3-sonnet-20240229',
+          'claude-3-5-sonnet-20240620',
           'claude-3-opus-20240229',
         },
         max_tokens = 128,

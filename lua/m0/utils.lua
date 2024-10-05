720de4e26a39a4ef7d0620b8f2badee433d4e24a
yk <federico@pietrolucci.net>
2024-10-05 20:23:36 +0200

    cleanup (utils): class signature.

--- lua/m0/utils.lua
+++ lua/m0/utils.lua
@@ -1,10 +1,4 @@
 ---@class M0.Utils
----@field new fun(self:M0.Utils, opts:table):M0.Utils
----@field opts table
----@field _logger fun(self:M0.Utils, level:integer):fun(message:string):nil
----@field log_error fun(self:M0.Utils, message: string):nil
----@field log_info fun(self:M0.Utils, message: string):nil
----@field log_warn fun(self:M0.Utils, message: string):nil
 ---@field safe_call fun(self:M0.Utils, func: fun(), ...):any
 ---@field json_decode fun(self:M0.Utils, data: any):any
 

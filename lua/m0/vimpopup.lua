769003aa90505a5e2a60f47e3ca556215866251e
yk <federico@pietrolucci.net>
2024-10-07 20:30:46 +0200

    feat (vimpopup): adds cleanup callback and popup at cursor.

--- lua/m0/vimpopup.lua
+++ lua/m0/vimpopup.lua
@@ -1,9 +1,12 @@
 local M = {}
 
----@param txt string The text to show in the popup
----@return boolean success
+---@param txt string | string[] The text to show in the popup
+---@param exit_callback? fun()
+---@param opts? table
+---@return integer? buffer id
 ---@return string? error
-function M:popup(txt)
+function M:popup(txt, exit_callback, opts)
+  local opts = opts or {}
   local win_width = vim.api.nvim_win_get_width(0)
   local win_height = vim.api.nvim_win_get_height(0)
 
@@ -17,20 +20,21 @@ function M:popup(txt)
     return nil, 'Unable to create popup buffer.'
   end
   vim.api.nvim_buf_set_lines(
-    popup_buf_id,
+    buf_id,
     -2,
     -1,
     false,
     -- If the input contains multiple lines,
     -- split them as required by nvim_buf_get_lines()
-    vim.fn.split(txt, '\n', false)
+    type(txt) == 'string' and vim.fn.split(txt, '\n', false) or txt
   )
-  local win_id = vim.api.nvim_open_win(popup_buf_id, true, {
-    relative = 'win',
-    row = 5,
-    col = 5,
-    width = win_width - 10,
-    height = win_height - 10,
+  local win_id = vim.api.nvim_open_win(buf_id, true, {
+    relative = opts.cursor and 'cursor' or 'win',
+    row = opts.cursor and 1 or 5,
+    col = opts.cursor and 1 or 5,
+    width = opts.cursor and 30 or win_width - 10,
+    height = opts.cursor and 10 or win_height - 10,
+    -- FIXME: make these configurable.
     style = 'minimal',
     border = 'rounded',
   })
@@ -42,9 +46,11 @@ function M:popup(txt)
   -- Bind q to quit popup.
   vim.keymap.set('n', 'q', function()
     vim.api.nvim_win_close(win_id, true)
-    vim.api.nvim_buf_delete(popup_buf_id, {})
-  end, { buffer = popup_buf_id })
-  return true
+    vim.api.nvim_buf_delete(buf_id, {})
+    if exit_callback then
+      exit_callback()
+    end
+  end, { buffer = buf_id })
   return buf_id
 end
 

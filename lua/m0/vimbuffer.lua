25d498a6c2c664e27438f685225b449bb12f811a
yk <federico@pietrolucci.net>
2024-10-23 21:05:04 +0200

    fix (vimbuffer): corrected bug in streaming rewrite.
    Text is no longer replicated in some corner cases.

--- lua/m0/vimbuffer.lua
+++ lua/m0/vimbuffer.lua
@@ -1,6 +1,8 @@
 ---@class M0.VimBuffer
----@field opts M0.Config | nil
----@field buf_id integer
+---@field opts? M0.Config
+---@field buf_id? integer
+---@field win_id? integer
+---@field cursor? table
 ---@field new? fun(self:M0.VimBuffer, opts:table):M0.VimBuffer
 ---@field get_visual_selection_lines? fun(self:M0.VimBuffer):string[]
 ---@field get_messages? fun(self:M0.VimBuffer):string[]
@@ -8,31 +10,36 @@
 ---@field close_buffer? fun(self:M0.VimBuffer, mode:string)
 ---@field rewrite? fun(self:M0.VimBuffer, response:string, opts?:table):boolean,string?
 ---@field put_response? fun(self:M0.VimBuffer, response:string, opts?:table):boolean
----@field insert_lines? fun(self:M0.VimBuffer, lines:string[], opts?:table)
+---@field append_section_mark? fun(self:M0.VimBuffer)
 ---@field get_last_line? fun(self:M0.VimBuffer):string
 ---@field set_last_line? fun(self:M0.VimBuffer, txt:string)
 
 ---@type M0.VimBuffer
 local M = {
   opts = nil,
-  buf_id = 0,
+  win_id = nil,
+  buf_id = nil,
+  cursor = { nil, nil },
 }
 
 ---@param opts M0.Config The current configuration
 ---@return M0.VimBuffer
 function M:new(opts)
-  return setmetatable(
-    { buf_id = vim.api.nvim_get_current_buf(), opts = opts },
-    { __index = M }
-  )
+  return setmetatable({
+    buf_id = vim.api.nvim_get_current_buf(),
+    win_id = vim.api.nvim_get_current_win(),
+    opts = opts,
+  }, { __index = M })
 end
 
 function M:put_response(response, opts)
+  -- This method assumes that 'open_buffer()' has appended an empty line
+  -- to the end of the buffer.
   if not opts or opts.stream == false then
-    -- Not streaming, so getting whole lines.
+    -- Not streaming, so the response consists of full lines.
     self:set_last_line(response)
   else
-    -- Streaming, so getting partial lines.
+    -- Streaming, so the response consists of partial lines.
     self:set_last_line(self:get_last_line() .. response)
   end
   return true
@@ -45,6 +52,7 @@ local function get_visual_selection_line_span()
   local eline = vim.fn.line '.'
   return math.min(sline, eline) - 1, math.max(sline, eline)
 end
+
 ---Get the currently selected text.
 ---@return string[]
 function M:get_visual_selection_lines()
@@ -64,7 +72,16 @@ end
 
 function M:rewrite(txt)
   -- Streaming, so getting partial lines.
-  vim.api.nvim_put(vim.fn.split(txt, '\n', true), 'c', true, true)
+  vim.api.nvim_buf_set_text(
+    self.buf_id,
+    self.cursor[1] - 1,
+    self.cursor[2],
+    self.cursor[1] - 1,
+    self.cursor[2],
+    vim.fn.split(txt, '\n', true)
+  )
+  self.cursor = vim.api.nvim_win_get_cursor(self.win_id)
+  -- vim.api.nvim_put(vim.fn.split(txt, '\n', true), 'c', true, true)
   return true
 end
 
@@ -102,22 +119,10 @@ function M:get_messages()
   return messages
 end
 
---- Insert lines, by default at end of the buffer.
----@param lines string[]
-function M:insert_lines(lines, opts)
-  local start_line = -1
-  local end_line = -1
-
-  if opts and opts.start_line and opts.end_line then
-    start_line = opts.start_line
-    end_line = opts.end_line
-  end
-  vim.api.nvim_buf_set_lines(self.buf_id, start_line, end_line, false, lines)
-end
-
 --- Get the last line of the buffer.
 ---@return string
 function M:get_last_line()
+  -- Neovim manual: ... to get the last element use start=-2 and end=-1.
   return table.concat(vim.api.nvim_buf_get_lines(self.buf_id, -2, -1, false))
 end
 
@@ -135,21 +140,45 @@ function M:set_last_line(txt)
   )
 end
 
+function M:append_section_mark()
+  -- Line index -1 refers to the index past the end of the buffer.
+  vim.api.nvim_buf_set_lines(
+    self.buf_id,
+    -1,
+    -1,
+    false,
+    { self.opts.section_mark, '' }
+  )
+end
+
 --- Open/close a response.
 function M:open_buffer(mode)
+  -- Ensure that the response is sent to the win/buf the request originated from.
+  self.buf_id = vim.api.nvim_get_current_buf()
+  self.win_id = vim.api.nvim_get_current_win()
+
   if mode == 'chat' then
-    self:insert_lines { self.opts.section_mark, '' }
+    self:append_section_mark()
   elseif mode == 'rewrite' and in_visual_mode() then
     local startline, endline = get_visual_selection_line_span()
-    -- Replace the visually selected line range with an empty line.
+    -- Replace the selected line range with an empty line.
     -- This is the placeholder for the rewrite.
     vim.api.nvim_buf_set_lines(self.buf_id, startline, endline, false, { '' })
+    -- Move the cursor to the empty line we just created, preparing for
+    -- text to be inserted.
+    self.cursor = { startline + 1, 0 }
+    vim.api.nvim_win_set_cursor(self.win_id, self.cursor)
   end
 end
 
 function M:close_buffer(mode)
   if mode == 'chat' then
-    self:insert_lines { self.opts.section_mark, '' }
+    self:append_section_mark()
+    -- Move cursor to end of document.
+    vim.api.nvim_win_set_cursor(
+      self.win_id,
+      { vim.api.nvim_buf_line_count(self.buf_id), 0 }
+    )
   end
 end
 

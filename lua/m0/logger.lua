c3dcb1fe6104db91043577e9840dae69bae15242
yk <federico@pietrolucci.net>
2024-10-27 01:17:24 +0200

    fix (logger): made _logger() and opts private.

--- lua/m0/logger.lua
+++ lua/m0/logger.lua
@@ -1,12 +1,12 @@
 ---@class M0.Logger
 ---@field new fun(self:M0.Logger, opts:table):M0.Logger
----@field opts table
----@field _logger fun(self:M0.Logger, level:integer):fun(message:string?):nil
+---@field private opts table
 ---@field log_trace fun(self:M0.Logger, message: string?):nil
 ---@field log_debug fun(self:M0.Logger, message: string?):nil
 ---@field log_info fun(self:M0.Logger, message: string?):nil
 ---@field log_warn fun(self:M0.Logger, message: string?):nil
 ---@field log_error fun(self:M0.Logger, message: string?):nil
+---@field private _logger fun(self:M0.Logger, level:integer):fun(message:string?):nil
 
 ---@type M0.Logger
 ---@diagnostic disable-next-line: missing-fields
@@ -18,8 +18,7 @@ function M:new(opts)
   return setmetatable({ opts = opts }, { __index = M })
 end
 
-function M:_logger(level)
-  return function(message)
+local function _logger(self, level)
     if self.opts.log_level > level then
       return
     end
@@ -28,23 +27,23 @@ function M:_logger(level)
 end
 
 function M:log_trace(message)
-  self:_logger(vim.log.levels.TRACE)('[TRACE] ' .. message)
+  _logger(self, vim.log.levels.TRACE)('[TRACE] ' .. message)
 end
 
 function M:log_debug(message)
-  self:_logger(vim.log.levels.DEBUG)('[DEBUG] ' .. message)
+  _logger(self, vim.log.levels.DEBUG)('[DEBUG] ' .. message)
 end
 
 function M:log_info(message)
-  self:_logger(vim.log.levels.INFO)('[INFO] ' .. message)
+  _logger(self, vim.log.levels.INFO)('[INFO] ' .. message)
 end
 
 function M:log_warn(message)
-  self:_logger(vim.log.levels.WARN)('[WARN] ' .. message)
+  _logger(self, vim.log.levels.WARN)('[WARN] ' .. message)
 end
 
 function M:log_error(message)
-  self:_logger(vim.log.levels.ERROR)('[ERROR] ' .. message)
+  _logger(self, vim.log.levels.ERROR)('[ERROR] ' .. message)
 end
 
 return M
